aa8bb9704019c967131f5a3ccda6545b9894e763
John Kerfoot <johnkerfoot@gmail.com>
2021-04-28 10:34:15 -0400

    template for adding to PYTHONPATH on conda activate ENVIRONMENT

---
+++ setup/env/conda/activate.d/env_vars.sh
@@ -0,0 +1,9 @@
+# env_vars.sh template
+# 2021-04-28: kerfoot@marine.rutgers.edu
+# Template for setting bash environment variables that are set on conda activate ENVIRONMENT
+#
+# Example:
+# export OLDPYTHONPATH=$PYTHONPATH;
+# export PYTHONPATH=/path/to/package
+
+
---
+++ setup/env/conda/deactivate.d/env_vars.sh
@@ -0,0 +1,8 @@
+# env_vars.sh template
+# 2021-04-28: kerfoot@marine.rutgers.edu
+# Template for setting bash environment variables that are set on conda activate ENVIRONMENT
+#
+# Example:
+# export PYTHONPATH=$OLDPYTHONPATH;
+# unset OLDPYTHONPATH;
+
